df376d6c34dbd67e748f1f8b46db9299213d26f2
andres <christiandaza@gmail.com>
2019-10-24 13:55:13 -0500

    sistema experto funcional 100%

--- orm.py
+++ orm.py
@@ -4,13 +4,15 @@ import datetime
 """contraseña mysql portatil = Cdaza96!"""
 db = MySQLDatabase('t_pers', user='root',passwd='Cdaza96!')
 
+terminal = '>>> '
+
 def main():
     
     command = str(input('''
-        herramienta SE dd
+        ***********************************
         Sistema experto 
         [c]omenzar
-    '''))
+    >>>'''))
     if command.lower() == 'c':
         eval_gen()
 
@@ -18,23 +20,23 @@ def main():
      
 def eval_gen():
     comp = 's'
-    r1 = str(input(see_preg('1',Question)+'\n>>> '))
+    r1 = str(input(see_preg('1',Question)+'\n'+terminal))
     if r1 == comp:
         preguntas_eval('1', '2', '3', '4', '5', Question_esp, 'Instrumentación médica')
     else:
-        r2 = str(input(see_preg('2',Question)+'\n>>> '))
+        r2 = str(input(see_preg('2',Question)+'\n'+terminal))
         if r2.lower() == comp:
             preguntas_eval('6', '7', '8', '9', '10', Question_esp, 'Imagenología médica y procesamiento de imágenes')
         else:
-            r3 = str(input(see_preg('3',Question)+'\n>>> '))
+            r3 = str(input(see_preg('3',Question)+'\n'+terminal))
             if r3.lower() == comp:
                 preguntas_eval('11', '12', '13', '14', '15', Question_esp, 'Procesamiento digital de señales')
             else:
-                r4 = str(input(see_preg('4',Question)+'\n>>> '))
+                r4 = str(input(see_preg('4',Question)+'\n'+terminal))
                 if r4.lower() == comp:
                     preguntas_eval('16', '17', '18', '19', '20', Question_esp, 'Biomecánica y rehabilitación')
                 else:
-                    r5 = str(input(see_preg('5',Question)+'\n>>> '))
+                    r5 = str(input(see_preg('5',Question)+'\n'+terminal))
                     if r5.lower() == comp:
                         preguntas_eval('21', '22', '23', '24', '25', Question_esp, 'Ingeniería clínica hospitalaria y Gestión tecnológica')    
 
@@ -54,19 +56,19 @@ def see_preg(id_preg,n_table):
 def preguntas_eval(a, b, c, d, e, n_table,diagnostico):
     nombre = n_table.get(n_table.id == a)
     print(nombre.pregunta)
-    r1 = input()
+    r1 = input(terminal)
     nombre = n_table.get(n_table.id == b)
     print(nombre.pregunta)
-    r2 = input()
+    r2 = input(terminal)
     nombre = n_table.get(n_table.id == c)
     print(nombre.pregunta)
-    r3 = input()
+    r3 = input(terminal)
     nombre = n_table.get(n_table.id == d)
     print(nombre.pregunta)
-    r4 = input()
+    r4 = input(terminal)
     nombre = n_table.get(n_table.id == e)
     print(nombre.pregunta)
-    r5 = input()
+    r5 = input(terminal)
     if (r1 == r2) and (r3 == r4) and (r5 == r1):
         print('El campo de acción de la ingeniería biomédica es: {}.'.format(diagnostico))
     else:
@@ -76,11 +78,31 @@ def cargar_preguntas_esp():
     Question_esp.create_table()
 
     preguntas=[
-        '¿1?', '¿2?', '¿3?', '¿4?', '¿5?',
-        '¿6?', '¿7?', '¿8?', '¿9?', '¿10?',
-        '¿11?', '¿12?', '¿13?', '¿14?', '¿15?',
-        '¿16?', '¿17?', '¿18?', '¿19?', '¿20?',
-        '¿21?', '¿22?', '¿23?', '¿24?', '¿25?'
+        '¿Desarrollar herramientas, metodos y procedimientos menos invasivos, que permitan una recuperación mucho mas rapida?',
+        '¿Realizar investigaciones en nanotecnología para disponer de herramientas que aumentan la precisión de las mediciones?',
+        '¿Desarrollar herramientas que permitan la identificación de la variabilidad en los dignosticos persona a persona?',
+        '¿Comprender los requisitos de comtatibilidad y aspectos de seguridad de los sistemas biomédicos?',
+        '¿Crear sistemas biomédicos usando sensores específicos y dispositivos móviles?',
+        '¿Desarrollo e investigación para el mejoramiento de maquinas y procedimientos de rayos X?',
+        '¿Desarrollo e investigación para el mejoramiento de maquinas y procedimientos de ultrasonido?',
+        '¿Desarrollo e investigación para el mejoramiento de maquinas y procedimientos de resonancia magnética nuclear?',
+        '¿Diseñar procedimientos de adquisición, almacenamiento, técnicas de procesamiento de imágenes digitales?',
+        '¿Desarrollar procesamiento de imágenes a partir de tecnicas como convolución, filtraje no-lineal, detección de contorno?',
+        '¿Analizar las caracteristicas de las señales electrofisiológicas para obtener información conllevando a nuevos avances en fisiología y biofísica?',
+        '¿Desarrollar instrumentación que permita construir sensores y ropa inteligente?',
+        '¿Desarrollar herramientas que permitan limpiar y aumentar la calidad de las señales?',
+        '¿Realizar investigaciones que permitan el monitoreo de aspectos como (ritmo de sueño) identificando patologias como estrés y epilepsia?',
+        '¿Diseñar tratamientos que permitan combatir trastornos neurodegenerativos como el Alzheimer?',
+        '¿Desarrollo de dispositivos de asistencia a discapacidades visuales?',
+        '¿Desarrollo de dispositivos de asistencia a discapacidades auditivas?',
+        '¿Realizar invesigaciones de interfases cerebro-humano?',
+        '¿Realizar investigaciones para el analisis y decodificación de información neuronal?',
+        '¿Desarrollar protesis que permitan mayor movilidad y funcionalidad en las personas que las van a usar?',
+        '¿Garantizar el cumplimiento de las normas para la seguridad de los equipos y las instalaciones?',
+        '¿Investigar los accidentes y daños relativos a la instrumentación biomédica?',
+        '¿Cumplir con los procedimientos de medidas y de verificación estableciendo para la instrumentación biomédica?',
+        '¿Programar y dirigir la ejecución del mantenimiento preventivo para el equipamiento biomédico instalado,?',
+        '¿Realizar el mantenimiento correctivo del equipamiento que los requiera?'
     ]
       
     for i in range(0,25):
@@ -90,12 +112,12 @@ def cargar_preguntas_gen():
     Question.create_table()
 
     preguntas=[
-        '¿1?',
-        '¿2?',
-        '¿3?',
-        '¿4?',
-        '¿5?'
-            ]
+        '¿Tener acceso a la información, procedimientos, variabilidad para la creación de instrumentación biomédica?',
+        '¿Diseñar, desarrollar e implementar procedimientos que permitan procesar imágenes médicas?',
+        '¿Desarrollar aplicaciones que permitan la extracción y procesamiento de señales en el cuerpo humano?',
+        '¿Desarrollar herramientas que permitan mitigar las discapacidades de los pacientes por accidentes u otras causas?',
+        '¿Realizar la coordinación de las nuevas inversiones de tecnologías biomédicas?'
+    ]
       
     for i in range(0,5):
         create_preg(preguntas[i], Question)
@@ -103,8 +125,7 @@ def cargar_preguntas_gen():
 
 class Question(Model,object):
     pregunta = CharField(unique=True)
-    #email = CharField(index=True)
-    #created_date = DateTimeField(default=datetime.datetime.now)
+    
 
     class Meta:
         database = db
@@ -115,8 +136,7 @@ class Question(Model,object):
 
 class Question_esp(Model):
     pregunta = CharField(unique=True)
-    #email = CharField(index=True)
-    #created_date = DateTimeField(default=datetime.datetime.now)
+    
 
     class Meta:
         database = db
